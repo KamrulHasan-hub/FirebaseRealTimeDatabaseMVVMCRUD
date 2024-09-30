8a941546980aee8db3632defea998af456f77e4c
kamrul <kamrulhasankganj@gmail.com>
2024-09-30 15:25:42 +0600

    Initial commit

---
+++ app/src/main/java/com/example/firebaserealtimedatabasemvvmcrud/viewmodel/DataViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.firebaserealtimedatabasemvvmcrud.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.firebaserealtimedatabasemvvmcrud.model.Data
+import com.example.firebaserealtimedatabasemvvmcrud.repository.DataRepository
+
+class DataViewModel: ViewModel() {
+
+    private val dataRepository = DataRepository()
+    private var _dataList:MutableLiveData<List<Data>> =  dataRepository.fetchData()
+    val dataList:LiveData<List<Data>> = _dataList
+
+
+    private val _error = MutableLiveData<String?>(null)
+    val error:MutableLiveData<String?> get() = _error
+
+
+    fun addData(data: Data,onSucces:()-> Unit, onFailure:()-> Unit) {
+
+        dataRepository.addData(data)
+            .addOnSuccessListener { onSucces() }
+            .addOnFailureListener {
+                _error.value = it.message
+                onFailure()
+            }
+
+    }
+
+    fun updateData(data: Data){
+        dataRepository.updateData(data)
+    }
+
+    fun deleteData(data: Data, onSucces: () -> Unit, onFailure: () -> Unit){
+        dataRepository.deleteData(data)
+        onSucces()
+    }
+
+    fun handleDatabaseError(errorMessage:String?){
+        _error.value = errorMessage
+    }
+
+}
